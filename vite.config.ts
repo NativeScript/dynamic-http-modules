88cbac57cbbf1698a6303ca8b0a558fbb4c03f6f
Nathan Walker <walkerrunpdx@gmail.com>
2026-01-06 18:48:29 -0800

    chore: custom android activity example

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,19 @@
 import { defineConfig } from 'vite';
-import { typescriptConfig } from '@nativescript/vite';;
+import { typescriptConfig, appComponentsPlugin } from '@nativescript/vite';
 
-export default defineConfig(({ mode }) => typescriptConfig({ mode }));
+export default defineConfig(({ mode }) => {
+  const config = typescriptConfig({ mode });
+  
+  // Register custom Android Activity and Application classes
+  config.plugins!.push(
+    appComponentsPlugin({
+      appComponents: [
+        './app/custom-activity.android.ts',
+        './app/custom-application.android.ts'
+      ],
+      platform: 'android'
+    })
+  );
+  
+  return config;
+});
